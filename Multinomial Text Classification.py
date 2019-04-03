afaf06b37264d933bdf8d97f9eba8a15dcdf1231
josh-flori <joshman108@sbcglobal.net>
2019-04-03 07:23:56 -0500

    a

--- Multinomial Text Classification.py
+++ Multinomial Text Classification.py
@@ -7,7 +7,8 @@ Once fitted, the vectorizer has built a dictionary of feature indices:  The inde
 in the vocabulary is linked to its frequency in the whole training corpus.
 
 Text preprocessing, tokenizing and filtering of stopwords are all included in CountVectorizer, 
-which builds a dictionary of features and transforms documents to feature vectors.
+which builds a dictionary of features and transforms documents to feature vectors. It also
+lowers the words. <<<<<<<<<
 
 But word counts are not enough. We need Tf-idf.
 
@@ -56,7 +57,11 @@ stop_words = set(stopwords.words('english'))
 data=pd.read_excel('/users/josh.flori/desktop/t.xlsx')
 
 # removed stop words comments, I found performance to be different when I just relied on sklearn to remove them
-comments=[[" ".join([w.lower() for w in word_tokenize(re.sub(r'[^A-Za-z]',' ',data['comment'][i].split("------------------------------")[0].replace("\n",""))) if w not in stop_words])][0] for i in range(len(data['comment']))]
+comments=[[" ".join([w for w in word_tokenize(re.sub(r'[^A-Za-z]',' ',data['comment'][i].split("------------------------------")[0].replace("\n",""))) if w not in stop_words])][0] for i in range(len(data['comment']))]
+
+# You can also choose to not remove stopwords and leave that up to sklean
+comments=[[" ".join([w for w in word_tokenize(re.sub(r'[^A-Za-z]',' ',data['comment'][i].split("------------------------------")[0].replace("\n","")))])][0] for i in range(len(data['comment']))]
+
 
 # test/train split
 X_train, X_test, y_train, y_test = train_test_split(comments, data['yes_no'])
@@ -94,7 +99,16 @@ text_classifier = Pipeline([
 
 text_classifier.fit(X_train, y_train)
 predicted=text_classifier.predict(X_test)
+# accuracy
 np.mean(predicted == y_test)
+
+
+
+
+"""""""""""""""""""""""""""""""""""
+"""" REPEAT N TIMES (OPTIONAL) """"
+"""""""""""""""""""""""""""""""""""
+sample_many(text_classifier,n)
 
 
 
@@ -114,6 +128,7 @@ tfidf_transformer = TfidfTransformer()
 X_train_counts = count_vect.fit_transform(X_train)
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 
+
 X_train_tf = tf_transformer.transform(X_train_counts)
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
@@ -125,84 +140,11 @@ X_train_tfidf = TfidfTransformer().fit_transform(count_vect.fit_transform(X_trai
 
 
 
-
-
-
-
 # fit the classifier.
 classifier = MultinomialNB().fit(X_train_tfidf, y_train)
 predicted = classifier.predict(X_test)
 accuracy=[predicted[i][0]==y_test.tolist()[i] for i in range(len(predicted))].count(True)/len(predicted)
 
-
-# if you want to know what the feature vectors actually look like in X_train_tfidf, one way to view that data is as follows.
-# feature_vectors_list will hold features for the data like this: [[X_train[0]],[X_train[1]],[X_train[2]],[X_train[3]]...]
-# and the length of each sublist X_train[i] will be equal to the vocabulary size, in this case it should be len(set(comments))
-# it would look something like this [[0.0, 0.57, 0.0, 0.57, 0.57], where each entry corresponds to a word in the vocabulary and the number 
-# is the calculated td-if for that word in that training example.
-feature_vectors_list = []
-for i in count_vect.fit_transform([i for i in data['comment']]):
-    feature_vectors_list.append(list(i.A[0]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-x=[i for i in data['comment']]
-y=data['yes_no']
-
-from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(x)
-X_train_counts.shape
-
-# observe that you get a result like:
-# [4, 1]
-# [1, 5]
-# [4, 1]
-# where rows correspond to rows in x, and length is = total vocab - removed stopwords. the integers are the counts of that vocab word in that training example
-for i in X_train_counts:
-    print(list(i.A[0]))
-
-
-from sklearn.feature_extraction.text import TfidfTransformer
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-X_train_tfidf = tf_transformer.transform(X_train_counts)
-X_train_tfidf.shape
-
-# observe that you get a result like:
-# [0.970, 0.242]
-# [0.196, 0.980]
-# [0.970, 0.242]
-# where rows correspond to rows in x, and length is = total vocab - removed stopwords. the numbers are the td-idf for that word in that class (confirmed for that class)
-for i in X_train_tf:
-    print(list(i.A[0]))
-
-
-# duplicate of information above.... but i think you... may need to use the above method in order to keep the transformer information and fit new data????
-# tfidf_transformer = TfidfTransformer()
-# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-# X_train_tfidf.shape
-#
-# for i in X_train_tfidf:
-#     print(list(i.A[0]))
 
 
 from sklearn.naive_bayes import MultinomialNB
@@ -218,39 +160,61 @@ predicted = clf.predict(X_new_tfidf)
 
 
 
-
-
-
-data=pd.read_excel('/users/josh.flori/desktop/t.xlsx')
-x=[i for i in data['comment']]
-X_new_counts = count_vect.transform(x)
-for i in X_new_counts:
-    print(list(i.A[0]))
     
     
     
-# ok just jotting down thoughts here.....  the way this works
+----------------------    
+    
+# The first line below instantiates a CountVectorizer. The second line will actually change the value of that count_vect. Count_vect inherents the original train vocabulary and the model parameters will be learned against it. It must be passed to the new test data to create word counts on that data. From both train and test, it must be passed into a tfidf transfomer to generate tfidf data.
+# CountVectorizer filters stopwords, lowers words and tokenizes words.
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(x_train)
-# is that... the second line will actually change the value of count_vect. the first line instantiates it, i think, then the second line sets count_vect to learn the vocalubalry of x_train. so that when you go to do something like this 
+
+# Run this:
+for i in X_train_counts:
+    print(list(i.A[0]))
+# And observe that you get a result like:
+# [4, 1...]
+# [1, 5...]
+# [4, 1...]
+# Where rows correspond to rows/training_examples in X_train and length of each row is = (total vocab - removed stopwords). The integers are the counts of that vocab word in that training example.
+
+# To take count data and get tfidf data from it, run:
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tf_transformer.fit_transform(X_train_counts)
+
+# And run this:
+for i in X_train_tf:
+    print(list(i.A[0]))
+# And observe that you get a result like:
+# [0.970, 0.242...]
+# [0.196, 0.980...]
+# [0.970, 0.242...]
+# where rows correspond to rows in x, and length is = (total vocab - removed stopwords). The numbers are the td-idf for that word.
+
+
+
+# Fit the classifier.
+classifier = MultinomialNB().fit(X_train_tfidf, y_train)
+predicted = classifier.predict(X_test)
+
+
+
 count_vect.transform(x_test)
 # any new words in x_test not in x_train will not be included in the output (confirmed!)
-# all that will happen is that for each test example you will get the tfidf for each word in the x_train vocabulary as it presents or does not present itself in x_test... which i guess makes sense. so you will want to run a test that checks how many words in new data were not in vocabulary. presumably not much but im sure at some point you will see a lot of new words on the basis of different threads have different keywords.
+# all that will happen is that for each test example you will be returned an array the length of the train vocab, and in each spot of that array is the count of that train vocab word in the x_test example... which i guess makes sense. so you will want to run a test that checks how many words in new data were not in vocabulary. presumably not much but im sure at some point you will see a lot of new words on the basis of different threads have different keywords.
 
 # so the important components here are count_vect which inherents the original vocabulary and which the parameters will be learned against, it must be passed to the new test data to create counts, then from the counts you need to create tdidf information using
-# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-# tfidf_transformer will create an output identical in size to count_vect.transform(x), it will just output tfidf instead of counts.
-# so again, train count_vect to have training vocab size and order by calling
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(x_train)
-# then get tfidf training data like 
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-# then at test time get new data based on vocab length and order or training data like
+# tfidf_transformer will create an output identical in size to count_vect.transform(x), it will just output tfidf instead of counts.
+
+# you can run your model, then generate prediction on new data by doing:
 X_test_counts = count_vect.transform(x_test)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
-# THEN YOU can process that through the fitted model to get predictions, the only question i have now is...... how does tfidf actually work in a naive bayes classifier, is it plugged into the exact same model
-
+# Then you can process that through the fitted model to get predictions
+classifier = MultinomialNB().fit(X_train_tfidf, y_train)
+predicted = classifier.predict(X_test)
 
 
 
